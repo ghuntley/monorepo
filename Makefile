7d035bab9d2a5b7c93104149d9f7993e76d416f0
Vincent Ambo <vincent@kivra.com>
2016-02-08 21:47:32 +0100

    Initial check-in

---
+++ Makefile
@@ -0,0 +1,8 @@
+PROJECT = stomp.erl
+PROJECT_DESCRIPTION = New project
+PROJECT_VERSION = 0.0.1
+
+# Whitespace to be used when creating files from templates.
+SP = 4
+
+include erlang.mk
---
+++ src/stomp.erl_app.erl
@@ -0,0 +1,11 @@
+-module(stomp.erl_app).
+-behaviour(application).
+
+-export([start/2]).
+-export([stop/1]).
+
+start(_Type, _Args) ->
+    stomp.erl_sup:start_link().
+
+stop(_State) ->
+    ok.
