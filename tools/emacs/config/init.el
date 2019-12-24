ceaa0ff0bafd1efeb154b982cc6db8a4907029f1
Vincent Ambo <tazjin@google.com>
2019-12-24 15:43:24 +0100

    feat(emacs.d): Advise split-window-below to call edwina-arrange

--- tools/emacs/config/init.el
+++ tools/emacs/config/init.el
@@ -63,7 +63,9 @@
               :caller 'ivy-switch-buffer))
   :bind (:map edwina-mode-map
               ("s-w b" . #'edwina-split-to-buffer))
-  :config (edwina-mode 1))
+  :config
+  (advice-add 'split-window-below :after 'edwina-arrange)
+  (edwina-mode 1))
 
 (use-package gruber-darker-theme)
 (use-package ht)
