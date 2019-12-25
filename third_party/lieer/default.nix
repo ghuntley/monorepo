3c94625a5fa352ccc73a82f554346ad00a176f25
Vincent Ambo <tazjin@google.com>
2019-12-25 14:09:09 +0100

    chore(lieer): Remove OAuth client patch
    This is now done in my work-specific configuration, which is
    elsewhere.

--- third_party/lieer/default.nix
+++ third_party/lieer/default.nix
@@ -7,18 +7,7 @@
 
 with pkgs.third_party;
 
-let
-  # My employer does not allow third-party projects to use our email
-  # accounts, but I want to use lieer for it anyways.
-  #
-  # To accomplish this the following derivation creates a patch that
-  # replaces the API client with one that I control.
-  authPatch = runCommand "client_secret.patch" {} ''
-    export PATH=${lib.makeBinPath [ gettext ]}:$PATH
-    export CLIENT_SECRET='${builtins.getEnv "LIEER_CLIENT_SECRET"}'
-    cat ${./api_client.patch} | envsubst > $out
-  '';
-in python3Packages.buildPythonApplication rec {
+python3Packages.buildPythonApplication rec {
   name = "lieer-${version}";
   version = "1.0";
 
@@ -28,8 +17,6 @@ in python3Packages.buildPythonApplication rec {
     rev = "v${version}";
     sha256 = "1zzylv8xbcrh34bz0s29dawzcyx39lai8y8wk0bl4x75v1jfynvf";
   };
-
-  patches = [ authPatch ];
 
   propagatedBuildInputs = with python3Packages; [
     notmuch
