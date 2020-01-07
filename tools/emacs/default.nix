9e2f86d65dd5f5047667f5a8f0aad5b8cb502701
Vincent Ambo <Vincent Ambo>
2020-01-07 22:28:36 +0000

    chore(emacs): Bump emacs-libvterm to a more recent version

--- tools/emacs/default.nix
+++ tools/emacs/default.nix
@@ -83,12 +83,20 @@ let
     transient
     use-package
     uuidgen
-    vterm
     web-mode
     websocket
     which-key
     xelb
     yaml-mode
+
+    (vterm.overrideAttrs(_: {
+      src = third_party.fetchFromGitHub{
+        owner = "akermu";
+        repo = "emacs-libvterm";
+        rev = "58b4cc40ee9872a08fc5cbfee78ad0e195a3306c";
+        sha256 = "1w5yfl8nq4k7xyldf0ivzv36vhz3dwdzk6q2vs3xwpx6ljy52px6";
+      };
+    }))
   ]) ++
 
   # Custom packages
