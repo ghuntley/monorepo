7edbcf9c04b121913198a436d2b1843107557c3c
Vincent Ambo <Vincent Ambo>
2020-01-09 01:05:47 +0000

    feat(third_party/lisp): Add derivation for bordeaux-threads

---
+++ third_party/lisp/bordeaux-threads.nix
@@ -0,0 +1,20 @@
+# This library is meant to make writing portable multi-threaded apps
+# in Common Lisp simple.
+{ pkgs, ... }:
+
+let src = builtins.fetchGit {
+  url = "https://github.com/sionescu/bordeaux-threads.git";
+  rev = "499b6d3f0ce635417d6096acf0a671d8bf3f6e5f";
+};
+in pkgs.nix.buildLisp.library {
+  name = "bordeaux-threads";
+  deps = [ pkgs.third_party.lisp.alexandria ];
+
+  srcs = map (f: src + ("/src/" + f)) [
+    "pkgdcl.lisp"
+    "bordeaux-threads.lisp"
+    "impl-sbcl.lisp"
+    "condition-variables.lisp"
+    "default-implementations.lisp"
+  ];
+}
