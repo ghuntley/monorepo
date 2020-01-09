06362a812ecec867b88c59e8565da1b43a39adb0
Vincent Ambo <Vincent Ambo>
2020-01-09 00:17:44 +0000

    feat(third_party/lisp): Add derivation for trivial-features

---
+++ third_party/lisp/trivial-features.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+
+let src = builtins.fetchGit {
+  url = "https://github.com/trivial-features/trivial-features.git";
+  rev = "b78b2df5d75bdf8fdfc69f0deec0a187d9664b0b";
+};
+in pkgs.nix.buildLisp.library {
+  name = "trivial-features";
+  srcs = [
+    (src + "/src/tf-sbcl.lisp")
+  ];
+}
