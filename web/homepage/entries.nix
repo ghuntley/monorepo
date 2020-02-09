69b6ff26c340305ba5abf13ef294e1533388cf09
Vincent Ambo <tazjin@google.com>
2020-02-09 00:12:22 +0000

    feat(web/homepage): Add entries for nixery, kontemplate, dottime

--- web/homepage/entries.nix
+++ web/homepage/entries.nix
@@ -1,1 +1,23 @@
-[]
+[
+  {
+    class = "project";
+    title = "Nixery";
+    url = "https://github.com/google/nixery";
+    date = 1565132400;
+    description = "A Nix-backed container registry that builds container images on demand";
+  }
+  {
+    class = "project";
+    title = "kontemplate";
+    url = "https://git.tazj.in/about/ops/kontemplate";
+    date = 1486550940;
+    description = "Simple file templating tool built for Kubernetes resources";
+  }
+  {
+    class = "misc";
+    title = "dottime";
+    url = "https://dotti.me/";
+    date = 1560898800;
+    description = "A universal convention for conveying time (by edef <3)";
+  }
+]
