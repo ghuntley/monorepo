f2235ba0aed43ad7a161d83b744e26b5acda3c85
Vincent Ambo <tazjin@google.com>
2020-02-21 12:48:07 +0000

    feat(build): Add //fun and //ops/nixos projects to CI builds

--- ci-builds.nix
+++ ci-builds.nix
@@ -4,24 +4,29 @@
 # automatically and detect all derivations that have `meta.enableCI =
 # true`, but this is currently more effort than it would save me.
 
-let
-  pkgs = import ./default.nix {};
-in with pkgs; [
+with (import ./default.nix {}); [
+  fun.amsterdump
   fun.gemma
+  fun.quinistry
+  fun.watchblob
+  fun.wcl
+  lisp.dns
+  nix.buildLisp.example
   nix.yants.tests
   ops."posix_mq.rs"
   ops.journaldriver
   ops.kms_pass
   ops.kontemplate
   ops.mq_cli
+  ops.nixos.camdenSystem
+  ops.nixos.nuggetSystem
   ops.sync-gcsr
-  tools.cheddar
-  tools.emacs
-  web.blog
-  web.cgit-taz
-  lisp.dns
   third_party.cgit
   third_party.git
   third_party.guile
   third_party.lisp # will build all third-party libraries
+  tools.cheddar
+  tools.emacs
+  web.blog
+  web.cgit-taz
 ]
