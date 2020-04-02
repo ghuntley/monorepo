f99fc009fce8a2d49af31792f72e6f9ef38d037d
Vincent Ambo <tazjin@google.com>
2020-04-02 21:59:18 +0100

    fix(tools/emacs): Fix notmuch-show-open-or-close-subthread
    Accidentally dropped a key parameter, also restored interactive
    functionality via prefix arguments..

--- tools/emacs/config/functions.el
+++ tools/emacs/config/functions.el
@@ -290,14 +290,14 @@
      (-map #'car tag-counts) "Tag:"
      (-map #'cdr tag-counts) "Count:")))
 
-(defun notmuch-show-open-or-close-subthread (&optional open)
+(defun notmuch-show-open-or-close-subthread (&optional prefix)
   "Open or close the subthread from (and including) the message at point."
-  (interactive)
+  (interactive "P")
   (save-excursion
     (let ((current-depth (map-elt (notmuch-show-get-message-properties) :depth 0)))
-      (loop do (notmuch-show-message-visible (notmuch-show-get-message-properties) open)
+      (loop do (notmuch-show-message-visible (notmuch-show-get-message-properties) prefix)
             until (or (not (notmuch-show-goto-message-next))
-                      (= (map-elt (notmuch-show-get-message-properties)) current-depth)))))
+                      (= (map-elt (notmuch-show-get-message-properties) :depth) current-depth)))))
   (force-window-update))
 
 (provide 'functions)
